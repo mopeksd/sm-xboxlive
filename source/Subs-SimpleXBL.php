63c5b16d470e8cc59d6d237724db55aa5ed8de80
Jason Clemons <jason@xboxleaders.com>
2014-01-30 16:36:23 -0500

    Update Subs-SimpleXBL.php

--- source/Subs-SimpleXBL.php
+++ source/Subs-SimpleXBL.php
@@ -1,1 +1,18 @@
 <?php
+/**
+* This file is the workhorse of SimpleXBL, and contains all
+* helper and critical functions. It is located in
+* ./Sources/Subs-SimpleXBL.php
+*
+* @package    SMF
+* @author     Jason Clemons <jason@xboxleaders.com>
+* @copyright  2014 Jason Clemons <jason@xboxleaders.com>
+* @license    http://opensource.org/licenses/mit-license.html MIT
+* @version    1.0
+* @link       https://github.com/jasonclemons/SimpleXBL
+* @since      File available since 1.0
+*/
+
+if (!defined('SMF')) {
+  die('Hacking attempt...');
+}
