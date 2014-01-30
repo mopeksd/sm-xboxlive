71060517712774f109c4333c4fe8724233435281
Jason Clemons <jason@xboxleaders.com>
2014-01-30 16:35:35 -0500

    Update SimpleXBL.php

--- source/SimpleXBL.php
+++ source/SimpleXBL.php
@@ -1,1 +1,17 @@
 <?php
+/**
+* This file works on displaying the leaderboard and all public
+* features. It is located in ./Sources/SimpleXBL.php
+*
+* @package    SMF
+* @author     Jason Clemons <jason@xboxleaders.com>
+* @copyright  2014 Jason Clemons <jason@xboxleaders.com>
+* @license    http://opensource.org/licenses/mit-license.html MIT
+* @version    1.0
+* @link       https://github.com/jasonclemons/SimpleXBL
+* @since      File available since 1.0
+*/
+
+if (!defined('SMF')) {
+  die('Hacking attempt...');
+}
