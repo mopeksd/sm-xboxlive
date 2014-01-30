9797b054c448e12f0c255e951e4ff793088f66a7
Jason Clemons <jason@xboxleaders.com>
2014-01-30 16:34:26 -0500

    Update ManageSimpleXBL.php

--- source/ManageSimpleXBL.php
+++ source/ManageSimpleXBL.php
@@ -1,1 +1,17 @@
 <?php
+/**
+* This file allows the ability to modify features and settings
+* of SimpleXBL, and is located in ./Sources/ManageSimpleXBL.php
+*
+* @package    SMF
+* @author     Jason Clemons <jason@xboxleaders.com>
+* @copyright  2014 Jason Clemons <jason@xboxleaders.com>
+* @license    http://opensource.org/licenses/mit-license.html MIT
+* @version    1.0
+* @link       https://github.com/jasonclemons/SimpleXBL
+* @since      File available since 1.0
+*/
+
+if (!defined('SMF')) {
+  die('Hacking attempt...');
+}
