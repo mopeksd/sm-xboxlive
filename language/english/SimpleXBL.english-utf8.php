4b67d311d5348007feaa1995b3c02e3d6584b80a
Jason Clemons <jason@xboxleaders.com>
2014-01-30 16:39:57 -0500

    Update SimpleXBL.english-utf8.php

--- language/english/SimpleXBL.english-utf8.php
+++ language/english/SimpleXBL.english-utf8.php
@@ -1,1 +1,17 @@
 <?php
+/**
+* This file contains all English language strings. It is
+* located in ./Themes/default/language/SimpleXBL.english.php
+*
+* @package    SMF
+* @author     Jason Clemons <jason@xboxleaders.com>
+* @copyright  2014 Jason Clemons <jason@xboxleaders.com>
+* @license    http://opensource.org/licenses/mit-license.html MIT
+* @version    1.0
+* @link       https://github.com/jasonclemons/SimpleXBL
+* @since      File available since 1.0
+*/
+
+if (!defined('SMF')) {
+  die('Hacking attempt...');
+}
